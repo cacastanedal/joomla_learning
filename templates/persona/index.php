3c2ae9f9685fa0d885db1cfe85758cf96fe3b1e4
carlosacastanedal <carlosacastanedal@gmail.com>
2017-11-21 16:49:46 -0500

    Basic template created

--- templates/persona/index.php
+++ templates/persona/index.php
@@ -1,1 +1,21 @@
-index.php
+<?php defined('_JEXEC') or die('Restricted access');?>
+
+<!DOCTYPE html>
+
+<html xml:lang="<?php echo $this->language; ?>" 
+			lang="<?php echo $this->language; ?>" >
+
+<head>
+	<jdoc:include type="head" />
+	<link rel="stylesheet" 
+				href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template ?>/css/template.css" 
+				type="text/css" />
+</head>
+
+<body>
+	<jdoc:include type="modules" name="top" /> 
+	<jdoc:include type="component" />
+	<jdoc:include type="modules" name="footer" />
+</body>
+
+</html>
